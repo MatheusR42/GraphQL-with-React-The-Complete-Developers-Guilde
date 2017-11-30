5ae916e4e4a060adb9c5959e2c3b4082c142744d
MatheusR42 <matheus.araujo.r42@gmail.com>
2017-11-30 09:30:05 -0200

    Nested queries

--- schema/schema.js
+++ schema/schema.js
@@ -7,13 +7,29 @@ const {
 	GraphQLSchema
 } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+	name: 'Company',
+	fields: {
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString }		
+	}
+});
 
 const UserType = new GraphQLObjectType({
 	name: 'User',
 	fields: {
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
-		age: { type: GraphQLInt }
+		age: { type: GraphQLInt },
+		company: {
+			type: CompanyType,
+			resolve(parentValue, args) {
+				console.log(parentValue)
+				return axios.get(`http://127.0.0.1:3000/companies/${parentValue.companyId}`)
+					.then(res => res.data);
+			}
+		}
 	}
 });
 
